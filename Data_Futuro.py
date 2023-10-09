ce0120d6762c68d317aaf01513d553b914a02e46
cristianorohling <122020011+cristianorohling@users.noreply.github.com>
2023-10-09 06:30:54 -0300

    Delete Data_Futuro.py

--- Data_Futuro.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime, timedelta
-
-data_atual = datetime.now()
-
-print(data_atual)
-
-dias_futuro = float(input("Digite o numero de dias: "))
-
-print(data_atual + timedelta(dias_futuro) )
